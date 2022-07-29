7ae0760d488c05fb1de071ab056829432932c377
TheMadIZM <94954286+TheMadIZM@users.noreply.github.com>
2022-07-29 03:12:56 -0400

    init8

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,9 +8,9 @@ import "../styles/globals.css";
 import "../styles/App.css";
 
 // set custom RPC server endpoint for the final website
-// const endpoint = "https://api.mainnet-beta.solana.com";
+// const endpoint = "https://ssc-dao.genesysgo.net/";
 // const endpoint = "http://127.0.0.1:8899";
-const endpoint = "https://api.mainnet-beta.solana.com";
+const endpoint = "https://ssc-dao.genesysgo.net/";
 
 const WalletProvider = dynamic(
   () => import("../contexts/ClientWalletProvider"),
