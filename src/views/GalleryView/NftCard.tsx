3aafb658eb930ee00a7166108237229f2b9cff3f
TheMadIZM <94954286+TheMadIZM@users.noreply.github.com>
2022-08-09 15:58:10 -0400

    madizm1

--- src/views/GalleryView/NftCard.tsx
+++ src/views/GalleryView/NftCard.tsx
@@ -5,19 +5,20 @@ import { EyeOffIcon } from "@heroicons/react/outline";
 import { fetcher } from "utils/fetcher";
 
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
-import { BurnButton } from '../../utils/BurnButton';
-
+import { SelectBurnButton } from '../../utils/SelectBurnButton';
 
 type Props = {
   details: any;
   onSelect: (id: string) => void;
   onTokenDetailsFetched?: (props: any) => unknown;
+  toBurn: any;
 };
 
 export const NftCard: FC<Props> = ({
   details,
   onSelect,
   onTokenDetailsFetched = () => { },
+  toBurn
 }) => {
   const [fallbackImage, setFallbackImage] = useState(false);
   const { name, uri } = details?.data ?? {};
@@ -36,7 +37,6 @@ export const NftCard: FC<Props> = ({
 
 
 
-
   useEffect(() => {
     if (!error && !!data) {
       onTokenDetailsFetched(data);
@@ -46,13 +46,25 @@ export const NftCard: FC<Props> = ({
   const onImageError = () => setFallbackImage(true);
   const { image } = data ?? {};
 
-  const tokenMintAddress = details.mint; 
+  const tokenMintAddress = details.mint;
 
-  const wallet = useWallet(); 
+  const wallet = useWallet();
 
   const { connection } = useConnection();
 
-  const { publicKey } = useWallet(); 
+  const { publicKey } = useWallet();
+
+  const creators = details.data.creators;
+
+  let firstCreator;
+
+  if (creators != undefined) {
+    firstCreator = details.data.creators[0].address;
+  }
+  else {
+    firstCreator = tokenMintAddress;
+  }
+
 
   return (
     <div className={`card bordered max-w-xs compact rounded-md`}>
@@ -61,20 +73,24 @@ export const NftCard: FC<Props> = ({
           <img
             src={image}
             onError={onImageError}
-            className="bg-black-800 object-cover"
+            className="bg-gray-900 object-cover h-30 lg:h-60"
           />
         ) : (
           // Fallback when preview isn't available
           // This could be broken image, video, or audio
-          <div className="w-auto h-48 flex items-center justify-center bg-gray-900 bg-opacity-40">
-            <EyeOffIcon className="h-16 w-16 text-orange-100" />
+          <div className="w-auto flex items-center justify-center bg-gray-900">
+            <EyeOffIcon className="h-16 w-16 text-white-500" />
           </div>
         )}
       </figure>
-      <div className="card-body">
-        <h2 className="card-title text-sm text-center">{name}</h2>
+      <div className="card-body h-28 sm:h-24">
+        <h2 className="card-title text-sm pt-4 text-center">{name}</h2>
       </div>
-      <BurnButton tokenMintAddress={tokenMintAddress} connection={connection} publicKey={publicKey} wallet={wallet} />
+      <div className="sm:flex justify-center">
+
+        <SelectBurnButton tokenMintAddress={tokenMintAddress} connection={connection} publicKey={publicKey} toBurn={toBurn} />
+        <a target="_blank" className="btn bg-[#55268e] hover:bg-[#3d1b66] uppercase mb-2 sm:mb-4 sm:mr-1" href={"https://solscan.io/token/" + tokenMintAddress}>Solscan</a>
+      </div>
     </div>
   );
 };
--- src/views/GalleryView/index.tsx
+++ src/views/GalleryView/index.tsx
@@ -1,33 +1,36 @@
 import Link from "next/link";
-import { FC, useState } from "react";
+import { Dispatch, FC, SetStateAction, useState } from "react";
 import { useWallet } from "@solana/wallet-adapter-react";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import { useWalletNfts, NftTokenAccount } from "@nfteyez/sol-rayz-react";
 import { useConnection } from "@solana/wallet-adapter-react";
 
-import { Loader, SolanaLogo, SelectAndConnectWalletButton } from "components";
+import { SolanaLogo, SelectAndConnectWalletButton } from "components";
 import { NftCard } from "./NftCard";
 import styles from "./index.module.css";
+import { BurnButton } from "utils/BurnButton";
 
 const walletPublicKey = "";
 
-export const GalleryView: FC = ({}) => {
+export const GalleryView: FC = ({ }) => {
   const { connection } = useConnection();
   const [walletToParsePublicKey, setWalletToParsePublicKey] =
     useState<string>(walletPublicKey);
   const { publicKey } = useWallet();
+  
+  const [refresh, setRefresh] = useState(false)
 
   const { nfts, isLoading, error } = useWalletNfts({
     publicAddress: walletToParsePublicKey,
     connection,
   });
 
-  console.log("nfts", nfts);
+  let errorMessage
+  if (error) {
+    errorMessage = error.message
+  }
 
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { value } = e.target;
-    setWalletToParsePublicKey(value.trim());
-  };
+  console.log("nfts", nfts);
 
   const onUseWalletClick = () => {
     if (publicKey) {
@@ -54,22 +57,13 @@ export const GalleryView: FC = ({}) => {
             <WalletMultiButton className="btn btn-ghost" />
           </div>
         </div>
-
+        
         <div className="text-center pt-2">
           <div className="hero min-h-16 p-0 pt-10">
             <div className="text-center hero-content w-full">
               <div className="w-full">
-                <h1 className="mb-20 text-6xl">
+                <h1 className="mb-5 text-5xl">
                   Burn unwanted <SolanaLogo /> NFTs!
-                </h1>
-                <h1 className="mb-4 text-m">
-                  This app will load token your token accounts, allow you to choose which ones you want to close, and return the rent! (Ususally about .002 SOL!)
-                </h1>
-                <h1 className="mb-14 text-m">
-                  Some accounts may not provide a title, picture, etc. Please allow larger wallets time to load, and please refresh if needed.
-                </h1>
-                <h1 className="mb-20 text-m">
-                Please be careful, and use at your discretion, as burns are irreversible.
                 </h1>
 
                 <div className="w-full min-w-full">
@@ -98,21 +92,23 @@ export const GalleryView: FC = ({}) => {
                     </div>
                   </div>
                 </div>
-                
+
                 <div className="mb-auto my-10">
-                  {error ? (
+                  {error && errorMessage != "Invalid address: " ? (
                     <div>
-                      <h1></h1>
+                      <h1>Error</h1>
+                      {(error as any)?.message}
                     </div>
                   ) : null}
 
-                  {!error && isLoading ? (
+                  {!error && isLoading &&
                     <div>
-                      <Loader />
                     </div>
-                  ) : (
-                    <NftList nfts={nfts} error={error} />
-                  )}
+                  }
+                  {!error && !isLoading && !refresh &&
+                    <NftList nfts={nfts} error={error} setRefresh={setRefresh} />
+                  }
+                  
                 </div>
               </div>
             </div>
@@ -126,9 +122,10 @@ export const GalleryView: FC = ({}) => {
 type NftListProps = {
   nfts: NftTokenAccount[];
   error?: Error;
+  setRefresh: Dispatch<SetStateAction<boolean>>
 };
 
-const NftList = ({ nfts, error }: NftListProps) => {
+const NftList = ({ nfts, error, setRefresh }: NftListProps) => {
   if (error) {
     return null;
   }
@@ -136,16 +133,31 @@ const NftList = ({ nfts, error }: NftListProps) => {
   if (!nfts?.length) {
     return (
       <div className="text-center text-2xl pt-16">
-        No tokens found...
+        No NFTs found...
       </div>
     );
   }
 
+  const NFTstoBurn: string[] = []
+  const { connection } = useConnection();
+  const { publicKey } = useWallet();
+  const wallet = useWallet();
+
   return (
-    <div className="grid grid-cols-2 md:grid-cols-4 gap-10 items-start">
-      {nfts?.map((nft) => (
-        <NftCard key={nft.mint} details={nft} onSelect={() => {}} />
-      ))}
+    <div>
+      <ul className="text-center font-semibold text-l mb-4">
+        <li className=" mb-6"><span className='text-[#16c60c] font-semibold'></span> The Iz-cinerator will load token your token accounts, allow you to choose which ones you want to close, and return the rent! (About .00204 SOL!)</li>
+        <li className=" mb-10"><span className='text-[#F03A17] font-semibold'></span>Some token accounts may not provide a title, picture, etc. Please refer to Solscan in order to ensure identity.</li>
+        <li className=" mb-16"><span className='text-[#ff7f00] font-bold'></span>Please be careful, and use at your discretion, as burns are irreversible.</li>
+      </ul>
+
+      <BurnButton toBurn={NFTstoBurn} connection={connection} publicKey={publicKey} wallet={wallet} setRefresh={setRefresh} />
+
+      <div className="grid grid-cols-2 pt-14 md:grid-cols-4 gap-14 items-start">
+        {nfts?.map((nft) => (
+          <NftCard key={nft.mint} details={nft} onSelect={() => { }} toBurn={NFTstoBurn} />
+        ))}
+      </div>
     </div>
   );
 };
